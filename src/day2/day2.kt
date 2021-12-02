f227e16dbc27dfb7e3b5f0919c9988fafbffdf31
tristanfazio <tristan.fazio@outlook.com.au>
2021-12-02 14:20:12 +0800

    finish day 2

--- src/day2/day2.kt
+++ src/day2/day2.kt
@@ -4,14 +4,44 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        var depth = 0
+        var hoz = 0
+        input.forEach {
+            val action = it.split(" ")[0]
+            val value = it.split(" ")[1].toInt()
+
+            when (action) {
+                "forward" -> hoz += value
+                "up" -> depth -= value
+                "down" -> depth += value
+                else -> return@forEach
+            }
+        }
+        return hoz * depth
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var depth = 0
+        var hoz = 0
+        var aim = 0
+        input.forEach {
+            val action = it.split(" ")[0]
+            val value = it.split(" ")[1].toInt()
+
+            when (action) {
+                "forward" -> {
+                    hoz += value
+                    depth += aim * value
+                }
+                "up" -> aim -= value
+                "down" -> aim += value
+                else -> return@forEach
+            }
+        }
+        return hoz * depth
     }
 
-    val input = readInput("Day02")
+    val input = readInput("day2/Day02")
     println(part1(input))
     println(part2(input))
 }
