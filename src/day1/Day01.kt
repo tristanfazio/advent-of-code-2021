b1303d2ae228e3a40d68b080bed07144c054e138
tristanfazio <tristan.fazio@outlook.com.au>
2021-12-02 10:18:21 +0800

    formatting

--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -7,21 +7,21 @@ fun main() {
         var counter: Int = 0;
         var prev: Int;
         var next: Int;
-        input.forEachIndexed() {index, element ->
+        input.forEachIndexed() { index, element ->
             prev = element.toInt()
 
             val nextIndex = index + 1
             if (nextIndex > input.size - 1) return@forEachIndexed
             next = input[nextIndex].toInt()
 
-            if(prev < next) counter++
+            if (prev < next) counter++
         }
         return counter
     }
 
     fun part2(input: List<String>): Int {
         var counter: Int = 0;
-        input.forEachIndexed() {index, element ->
+        input.forEachIndexed() { index, element ->
             //window 1
             val w1LeftIndex = index
             val w1MiddleIndex = index + 1
@@ -34,23 +34,23 @@ fun main() {
 
             //EOA guard
             val indices = listOf(w1LeftIndex, w1MiddleIndex, w1RightIndex, w2LeftIndex, w2MiddleIndex, w2RightIndex)
-            if(indices.any{ it > input.size -1}) return@forEachIndexed
+            if (indices.any { it > input.size - 1 }) return@forEachIndexed
 
             val window1Sum = input[w1LeftIndex].toInt() + input[w1MiddleIndex].toInt() + input[w1RightIndex].toInt()
             val window2Sum = input[w2LeftIndex].toInt() + input[w2MiddleIndex].toInt() + input[w2RightIndex].toInt()
-            if(window1Sum < window2Sum) counter++
+            if (window1Sum < window2Sum) counter++
         }
         return counter
     }
 
     fun part1refined(input: List<String>): Int {
         val list = input.map { it.toInt() }
-        return list.windowed(2).count {(a,b) -> a < b }
+        return list.windowed(2).count { (a, b) -> a < b }
     }
 
     fun part2refined(input: List<String>): Int {
         val list = input.map { it.toInt() }
-        return list.windowed(4).count {(a,b,c,d) -> a < d }
+        return list.windowed(4).count { (a, b, c, d) -> a < d }
     }
 
     val input = readInput("day1/Day01")
