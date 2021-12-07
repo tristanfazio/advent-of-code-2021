a5c575ad3a107afcabe928cac7f1f616b583e3ec
tristanfazio <tristan.fazio@outlook.com.au>
2021-12-07 12:59:21 +0800

    setup Day07

---
+++ src/day7/Day07.kt
@@ -0,0 +1,23 @@
+package day7
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+
+        return 0
+    }
+
+    val input = readInput("day7/Day07")
+    val test = readInput("day7/Day07_test")
+    println("test part1: ${part1(test)}")
+    println("test part2: ${part2(test)}")
+
+//    println("part1: ${part1(input)}")
+//    println("part2: ${part2(input)}")
+}
