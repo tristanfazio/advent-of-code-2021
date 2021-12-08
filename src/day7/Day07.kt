b9e74499cb6f0b7aa2be3d8a6b1b147da9e84fb3
tristanfazio <tristan.fazio@outlook.com.au>
2021-12-08 13:18:07 +0800

    finish day07

--- src/day7/Day07.kt
+++ src/day7/Day07.kt
@@ -1,23 +1,51 @@
 package day7
 
-import readInput
+import readInputAsString
+import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<String>): Int {
-
-        return 0
+    fun part1(input: String): Int {
+        //Hoz to Fuel
+        val fuelSpent = mutableMapOf<Int,Int>()
+        val split = input.split(",")
+        split.forEachIndexed { index, hozValue1 ->
+            if(fuelSpent.contains(hozValue1.toInt())) return@forEachIndexed
+            val mutableSplit = split.toMutableList()
+            mutableSplit.removeAt(index)
+            mutableSplit.forEach { hozValue2 ->
+                 fuelSpent[hozValue1.toInt()] = (fuelSpent[hozValue1.toInt()] ?: 0 ) + abs(hozValue2.toInt() - hozValue1.toInt())
+            }
+        }
+        return fuelSpent.values.toList().sortedWith(compareBy { it }).first()
+    }
+    fun part2(input: String): Int {
+        //Hoz to Fuel
+        val fuelSpent = mutableMapOf<Int,Int>()
+        val split = input.split(",")
+        val min = split.sortedWith(compareBy { it }).first().toInt()
+        val max = split.sortedWith(compareBy { it }).last().toInt()
+        for(i in min..max) {
+            val mutableSplit = split.toMutableList()
+            mutableSplit.forEach { hozValue2 ->
+                val distance = abs(hozValue2.toInt() - i)
+                fuelSpent[i] = (fuelSpent[i] ?: 0 ) + fuelSpent(distance)
+            }
+        }
+        return fuelSpent.values.toList().sortedWith(compareBy { it }).first()
     }
 
-    fun part2(input: List<String>): Int {
-
-        return 0
-    }
-
-    val input = readInput("day7/Day07")
-    val test = readInput("day7/Day07_test")
+    val input = readInputAsString("day7/Day07")
+    val test = readInputAsString("day7/Day07_test")
     println("test part1: ${part1(test)}")
     println("test part2: ${part2(test)}")
 
-//    println("part1: ${part1(input)}")
-//    println("part2: ${part2(input)}")
+    println("part1: ${part1(input)}")
+    println("part2: ${part2(input)}")
+}
+
+
+fun fuelSpent(distance: Int): Int {
+    if (distance <= 0)
+        return distance;
+    return fuelSpent(distance-1) + distance
 }
