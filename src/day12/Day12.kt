bb33e203cec888c12c7a7db35d22339a579b83da
tristanfazio <tristan.fazio@outlook.com.au>
2021-12-18 18:16:30 +0800

    start day12

---
+++ src/day12/Day12.kt
@@ -0,0 +1,33 @@
+package day12
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+
+        return 0
+    }
+
+    val input = readInput("day12/Day12")
+    val test = readInput("day12/Day12_test")
+
+    val partOneTest = part1(test)
+    println("test part1: $partOneTest")
+    check(partOneTest == 19)
+
+//    val partOneInput = part1(input)
+//    println("part1: $partOneInput")
+//    check(partOneInput == 1617)
+////
+//    val partTwoTest = part2(test)
+//    println("test part2: $partTwoTest")
+//    check(partTwoTest == 195)
+////
+//    val partTwoInput = part2(input)
+//    println("part2: $partTwoInput")
+}
